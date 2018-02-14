f7ddf968c4dc807e7e03c9fc889857f190f3630b
Rutth <ruth.barros12@gmail.com>
2018-02-14 18:43:08 -0300

    Camera QR CODE

--- mobile/src/main/java/com/digitalflow/belchior/appbelchior/Activity/HomeActivity.java
+++ mobile/src/main/java/com/digitalflow/belchior/appbelchior/Activity/HomeActivity.java
@@ -1,23 +1,36 @@
 package com.digitalflow.belchior.appbelchior.Activity;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Matrix;
 import android.hardware.usb.UsbInterface;
+import android.os.Vibrator;
 import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.SparseArray;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.view.WindowManager;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.content.Intent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.digitalflow.belchior.appbelchior.Entidades.Usuarios;
 import com.digitalflow.belchior.appbelchior.R;
+import com.google.android.gms.vision.CameraSource;
+import com.google.android.gms.vision.Detector;
+import com.google.android.gms.vision.barcode.Barcode;
+import com.google.android.gms.vision.barcode.BarcodeDetector;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -26,7 +39,33 @@ import java.io.InputStream;
 public class HomeActivity extends AppCompatActivity {
 
     ImageView imageView;
-    private Bitmap bitmap;
+    SurfaceView cameraPreview;
+    TextView txtResult;
+    BarcodeDetector barcodeDetector;
+    CameraSource cameraSource;
+    final int RequestCameraPermissionID = 1001;
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case RequestCameraPermissionID: {
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+
+                        return;
+                    }
+                    try {
+                        cameraSource.start(cameraPreview.getHolder());
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
+                }
+             }
+             break;
+         }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,17 +74,79 @@ public class HomeActivity extends AppCompatActivity {
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_home);
 
-        ImageButton btnCamera = (ImageButton)findViewById(R.id.btnCamera);
-        imageView = (ImageView)findViewById(R.id.imageView);
+//        ImageButton btnCamera = (ImageButton)findViewById(R.id.btnCamera);
+//        imageView = (ImageView)findViewById(R.id.imageView);
 
-        btnCamera.setOnClickListener(new View.OnClickListener() {
+//        btnCamera.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+////                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+////                startActivityForResult(intent,0);
+//                Usuarios user = Usuarios.getInstance();
+////                Toast.makeText(getApplicationContext(), user.getId(), Toast.LENGTH_SHORT).show();
+//
+//            }
+//        });
+        cameraPreview = (SurfaceView) findViewById(R.id.cameraPreview);
+        txtResult = (TextView) findViewById(R.id.txtResult);
+
+        barcodeDetector = new BarcodeDetector.Builder(this)
+                .setBarcodeFormats(Barcode.QR_CODE)
+                .build();
+
+        cameraSource = new CameraSource
+                .Builder(this, barcodeDetector)
+                .setRequestedPreviewSize(640, 480)
+                .build();
+
+        cameraPreview.getHolder().addCallback(new SurfaceHolder.Callback() {
             @Override
-            public void onClick(View view) {
-//                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-//                startActivityForResult(intent,0);
-                Usuarios user = Usuarios.getInstance();
-//                Toast.makeText(getApplicationContext(), user.getId(), Toast.LENGTH_SHORT).show();
-                abrirCamera();
+            public void surfaceCreated(SurfaceHolder surfaceHolder) {
+                if (ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(HomeActivity.this, new String[]{Manifest.permission.CAMERA}, RequestCameraPermissionID);
+                    return;
+                }
+                try{
+                    cameraSource.start(cameraPreview.getHolder());
+                } catch (IOException e){
+                    e.printStackTrace();
+                }
+
+            }
+
+
+            @Override
+            public void surfaceChanged(SurfaceHolder surfaceHolder, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
+                cameraSource.stop();
+            }
+        });
+        //END
+
+        barcodeDetector.setProcessor(new Detector.Processor<Barcode>() {
+            @Override
+            public void release() {
+
+            }
+
+            @Override
+            public void receiveDetections(Detector.Detections<Barcode> detections) {
+                final SparseArray<Barcode> qrcodes = detections.getDetectedItems();
+                if(qrcodes.size() != 0 ){
+                    txtResult.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            Vibrator vibrator = (Vibrator)getApplicationContext().getSystemService(Context.VIBRATOR_SERVICE);
+                            vibrator.vibrate(1000);
+                            txtResult.setText(qrcodes.valueAt(0).displayValue);
+                            Toast.makeText(HomeActivity.this, "oie", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                }
             }
         });
     }
@@ -57,57 +158,7 @@ public class HomeActivity extends AppCompatActivity {
 //        imageView.setImageBitmap(bitmap);
 //    }
 
-    public void abrirCamera(){
-        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        startActivityForResult(intent, 0);
-    }
 
-    protected void onActivityResult(int requestCode, int resultCode, Intent data){
-        super.onActivityResult(requestCode, resultCode, data);
-        InputStream stream = null;
-        if(requestCode == 0 && resultCode == RESULT_OK){
-            try{
-                if(bitmap != null){
-                    bitmap.recycle();
-                }
-                stream = getContentResolver().openInputStream(data.getData());
-                bitmap = BitmapFactory.decodeStream(stream);
-                imageView.setImageBitmap(resizeImage(this, bitmap, 700, 600));
-            } catch (FileNotFoundException e){
-                e.printStackTrace();
-            } finally {
-                if(stream != null){
-                    try{
-                        stream.close();
-                    } catch (IOException e){
-                        e.printStackTrace();
-                    }
-                }
-            }
-        }
-    }
-
-    private static Bitmap resizeImage(Context context, Bitmap bmpOriginal, float newWidth, float newHeight){
-        Bitmap novoBmp = null;
-        int w = bmpOriginal.getWidth();
-        int h = bmpOriginal.getHeight();
-
-        float densityFactor = context.getResources().getDisplayMetrics().density;
-        float novoW = newWidth * densityFactor;
-        float novoH = newHeight * densityFactor;
-
-        float scalaW = novoW / w;
-        float scalaH = novoH / h;
-
-        Matrix matrix = new Matrix();
-
-        matrix.postScale(scalaW, scalaH);
-
-        novoBmp = Bitmap.createBitmap(bmpOriginal, 0, 0, w, h, matrix, true);
-
-        return novoBmp;
-
-    }
 
 }
 
