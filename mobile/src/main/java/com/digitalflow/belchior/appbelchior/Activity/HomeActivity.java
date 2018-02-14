cc6af78bc7bb6c0df549500e9e37e231e23b17c4
Rutth <ruth.barros12@gmail.com>
2018-02-14 16:20:18 -0300

    Camera integrada

--- mobile/src/main/java/com/digitalflow/belchior/appbelchior/Activity/HomeActivity.java
+++ mobile/src/main/java/com/digitalflow/belchior/appbelchior/Activity/HomeActivity.java
@@ -1,6 +1,9 @@
 package com.digitalflow.belchior.appbelchior.Activity;
 
+import android.content.Context;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
 import android.hardware.usb.UsbInterface;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
@@ -16,9 +19,14 @@ import android.widget.Toast;
 import com.digitalflow.belchior.appbelchior.Entidades.Usuarios;
 import com.digitalflow.belchior.appbelchior.R;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+
 public class HomeActivity extends AppCompatActivity {
 
     ImageView imageView;
+    private Bitmap bitmap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,15 +44,69 @@ public class HomeActivity extends AppCompatActivity {
 //                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
 //                startActivityForResult(intent,0);
                 Usuarios user = Usuarios.getInstance();
-                Toast.makeText(getApplicationContext(), user.getId(), Toast.LENGTH_SHORT).show();
+//                Toast.makeText(getApplicationContext(), user.getId(), Toast.LENGTH_SHORT).show();
+                abrirCamera();
             }
         });
     }
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+//    @Override
+//    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+//        super.onActivityResult(requestCode, resultCode, data);
+//        Bitmap bitmap = (Bitmap)data.getExtras().get("data");
+//        imageView.setImageBitmap(bitmap);
+//    }
+
+    public void abrirCamera(){
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intent, 0);
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
         super.onActivityResult(requestCode, resultCode, data);
-        Bitmap bitmap = (Bitmap)data.getExtras().get("data");
-        imageView.setImageBitmap(bitmap);
+        InputStream stream = null;
+        if(requestCode == 0 && resultCode == RESULT_OK){
+            try{
+                if(bitmap != null){
+                    bitmap.recycle();
+                }
+                stream = getContentResolver().openInputStream(data.getData());
+                bitmap = BitmapFactory.decodeStream(stream);
+                imageView.setImageBitmap(resizeImage(this, bitmap, 700, 600));
+            } catch (FileNotFoundException e){
+                e.printStackTrace();
+            } finally {
+                if(stream != null){
+                    try{
+                        stream.close();
+                    } catch (IOException e){
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+    }
+
+    private static Bitmap resizeImage(Context context, Bitmap bmpOriginal, float newWidth, float newHeight){
+        Bitmap novoBmp = null;
+        int w = bmpOriginal.getWidth();
+        int h = bmpOriginal.getHeight();
+
+        float densityFactor = context.getResources().getDisplayMetrics().density;
+        float novoW = newWidth * densityFactor;
+        float novoH = newHeight * densityFactor;
+
+        float scalaW = novoW / w;
+        float scalaH = novoH / h;
+
+        Matrix matrix = new Matrix();
+
+        matrix.postScale(scalaW, scalaH);
+
+        novoBmp = Bitmap.createBitmap(bmpOriginal, 0, 0, w, h, matrix, true);
+
+        return novoBmp;
+
     }
 
 }
